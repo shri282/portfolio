f7f712b97839d045c21db94e13a828a183a29b78
shrikadhir <shrikadhir.k@softsuave.org>
2025-04-16 03:35:17 +0530

    auto scroll changes

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+function navHandler(event) {
+    const section = event.target.innerText;
+    let sectionDiv;
+
+    if (section === "About me") {
+        sectionDiv = document.getElementById("about-me");
+    } else if (section === "Skills") {
+        sectionDiv = document.getElementById("skills");
+    }
+
+    if (sectionDiv) {
+        const yOffset = -70; // header height
+        const y = sectionDiv.getBoundingClientRect().top + window.pageYOffset + yOffset;
+        window.scrollTo({ top: y, behavior: 'smooth' });
+    }
+}
 
 const getReadmore = function () {
     let readMoreFlag = false;
@@ -14,11 +30,21 @@ const getReadmore = function () {
 
 const readMore = getReadmore();
 
-window.addEventListener('scroll', () => {
+function toggleHeaderClass() {
     const header = document.getElementById('main-header');
     if (window.scrollY === 0) {
         header.classList.add('header-top');
     } else {
         header.classList.remove('header-top');
     }
+}
+
+window.addEventListener('scroll', () => {
+    toggleHeaderClass();
 });
+
+window.addEventListener('load', () => {
+    toggleHeaderClass();
+});
+
+
